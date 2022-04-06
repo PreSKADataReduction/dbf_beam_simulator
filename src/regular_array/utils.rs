ca2f45def3774cf138e9245ffc03c53708f2aeed
GU Junhua <jhgu@nao.cas.cn>
2022-04-06 13:23:29 +0800

    new file:   src/regular_array/utils.rs

---
+++ src/regular_array/utils.rs
@@ -0,0 +1,38 @@
+use ndarray::{s, Array1, Array2, ArrayView2};
+
+pub fn full2quarter<T>(full: ArrayView2<T>) -> Array2<T>
+where
+    T: Copy,
+{
+    let h = full.shape()[0];
+    let w = full.shape()[1];
+    full.slice(s![h / 2..h, w / 2..w]).to_owned()
+}
+
+pub fn quarter2full<T>(quarter: ArrayView2<T>) -> Array2<T>
+where
+    T: Copy + Default,
+{
+    let h = quarter.shape()[0] * 2;
+    let w = quarter.shape()[1] * 2;
+    let mut full = Array2::<T>::default((h, w));
+    full.slice_mut(s![h / 2..h, w / 2..w]).assign(&quarter);
+    full.slice_mut(s![1..h/2;-1, 1..w/2;-1])
+        .assign(&quarter.slice(s![1..h / 2, 1..w / 2]));
+    full.slice_mut(s![h/2..h, 1..w/2;-1])
+        .assign(&quarter.slice(s![.., 1..w / 2]));
+    full.slice_mut(s![1..h/2;-1, w/2..w])
+        .assign(&quarter.slice(s![1..h / 2, ..]));
+    full
+}
+
+pub fn flattern_quarter_wgt(wgt: ArrayView2<f64>) -> Vec<f64> {
+    wgt.iter().skip(1).cloned().collect()
+}
+
+pub fn deflattern_quarter_wgt(wgt: &[f64], h: usize, w: usize) -> Array2<f64> {
+    let one = [1.0];
+    Array1::from_iter(one.iter().chain(wgt.iter()).cloned())
+        .into_shape((h / 2, w / 2))
+        .unwrap()
+}
